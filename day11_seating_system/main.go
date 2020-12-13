dd5ed5aa26f0b0ba8459d363058a334b1716a3af
Joshua Nunn <joshuanunn@hotmail.co.uk>
2020-12-13 02:50:06 +0000

    tidy day 11

--- day11_seating_system/main.go
+++ day11_seating_system/main.go
@@ -13,32 +13,96 @@ const (
 	occupied = byte('#')
 )
 
-type rule func([][]byte, int, int, int, int) int
+type Seating struct {
+	initial [][]byte
+	final   [][]byte
+	rows    int
+	cols    int
+}
+
+type rule func(*Seating, int, int) int
 
 func main() {
 	// Read inputs into slice of int
 	input := readInputs("input.txt")
-	data := parseInputs(input)
 
 	// Part 1 - calculate number of 1 and 3 sequential diffs, and return product.
-	count, rounds := converge(data, rulePart1, 4, 1000)
+	seating := parseInputs(input)
+	count, rounds := converge(seating, rulePart1, 4, 1000)
 	fmt.Printf("Part 1 - total occupied seats: %d (after %d rounds)\n", count, rounds)
 
 	// Part 2 - find number of valid combinations of adaptors in input.
-	count, rounds = converge(data, rulePart2, 5, 1000)
+	seating = parseInputs(input)
+	count, rounds = converge(seating, rulePart2, 5, 1000)
 	fmt.Printf("Part 2 - total occupied seats: %d (after %d rounds)\n", count, rounds)
 }
 
-func converge(data [][]byte, f rule, tolerance, rounds int) (int, int) {
+func converge(seating *Seating, f rule, tolerance, rounds int) (int, int) {
 	for round := 1; round <= rounds; round++ {
-		data2, converged := sweep(data, f, tolerance)
+		converged := seating.sweep(f, tolerance)
 		if converged {
-			count := countOccupiedGrid(data)
+			count := seating.countOccupiedGrid()
 			return count, round
 		}
-		data = data2
 	}
 	return 0, rounds
+}
+
+// Function to calculate seating arrangements based on rule
+func (s *Seating) sweep(f rule, tolerance int) bool {
+	for y := 0; y < s.rows; y++ {
+		for x := 0; x < s.cols; x++ {
+			// Check current seat using appropriate ruleset
+			countOccupied := f(s, x, y)
+			if s.initial[y][x] == empty && countOccupied == 0 {
+				s.final[y][x] = occupied
+			} else if s.initial[y][x] == occupied && countOccupied >= tolerance {
+				s.final[y][x] = empty
+			}
+		}
+	}
+	// Convergence check - has grid changed?
+	status := s.checkGrid()
+	// update seating state
+	duplicateGrid(s.initial, s.final)
+	return status
+}
+
+func (s *Seating) show() {
+	for y := 0; y < len(s.initial); y++ {
+		for x := 0; x < len(s.initial[0]); x++ {
+			fmt.Printf("%c", s.initial[y][x])
+		}
+		fmt.Printf("\n")
+	}
+	fmt.Printf("\n")
+}
+
+func (s *Seating) checkGrid() bool {
+	var same int
+	for y := 0; y < s.rows; y++ {
+		for x := 0; x < s.cols; x++ {
+			if s.initial[y][x] == s.final[y][x] {
+				same++
+			}
+		}
+	}
+	if same == s.rows*s.cols {
+		return true
+	}
+	return false
+}
+
+func (s *Seating) countOccupiedGrid() int {
+	var total int
+	for y, row := range s.initial {
+		for x := range row {
+			if s.initial[y][x] == occupied {
+				total++
+			}
+		}
+	}
+	return total
 }
 
 func generateGrid(rows, cols int) [][]byte {
@@ -57,59 +121,36 @@ func duplicateGrid(dst, src [][]byte) {
 	}
 }
 
-func checkGrid(gridA, gridB [][]byte) bool {
-	var total, same int
-	for y, row := range gridB {
-		for x := range row {
-			total++
-			if gridA[y][x] == gridB[y][x] {
-				same++
-			}
-		}
-	}
-	if total == same {
-		return true
-	}
-	return false
-}
-
-func countOccupiedGrid(grid [][]byte) int {
-	var total int
-	for y, row := range grid {
-		for x := range row {
-			if grid[y][x] == occupied {
-				total++
-			}
-		}
-	}
-	return total
-}
-
-func rulePart1(initial [][]byte, x, y, cols, rows int) int {
+func rulePart1(s *Seating, x, y int) int {
 	var countOccupied int
-	for j := -1; j <= 1; j++ {
-		for i := -1; i <= 1; i++ {
+	i0, i1 := -1, 1
+	j0, j1 := -1, 1
+	if x == 0 {
+		i0 = 0
+	}
+	if x == s.cols-1 {
+		i1 = 0
+	}
+	if y == 0 {
+		j0 = 0
+	}
+	if y == s.rows-1 {
+		j1 = 0
+	}
+	for j := j0; j <= j1; j++ {
+		for i := i0; i <= i1; i++ {
 			if i == j && j == 0 {
 				continue
 			}
-			xx := x + i
-			yy := y + j
-			if xx < 0 || xx >= cols {
-				continue
-			}
-			if yy < 0 || yy >= rows {
-				continue
-			}
-			if initial[yy][xx] == occupied {
+			if s.initial[y+j][x+i] == occupied {
 				countOccupied++
 			}
-
 		}
 	}
 	return countOccupied
 }
 
-func rulePart2(initial [][]byte, x, y, cols, rows int) int {
+func rulePart2(s *Seating, x, y int) int {
 	var countOccupied int
 	// span all 8 compass directions
 	for dj := -1; dj <= 1; dj++ {
@@ -117,22 +158,19 @@ func rulePart2(initial [][]byte, x, y, cols, rows int) int {
 			if di == dj && dj == 0 {
 				continue
 			}
-			xx := x
-			yy := y
+			xx, yy := x, y
 			for {
-				xx += di
-				yy += dj
-
-				if xx < 0 || xx >= cols {
+				xx, yy = xx+di, yy+dj
+				if xx < 0 || xx >= s.cols {
 					break
 				}
-				if yy < 0 || yy >= rows {
+				if yy < 0 || yy >= s.rows {
 					break
 				}
-				if initial[yy][xx] == empty {
+				if s.initial[yy][xx] == empty {
 					break
 				}
-				if initial[yy][xx] == occupied {
+				if s.initial[yy][xx] == occupied {
 					countOccupied++
 					break
 				}
@@ -142,48 +180,20 @@ func rulePart2(initial [][]byte, x, y, cols, rows int) int {
 	return countOccupied
 }
 
-// Function to calculate seating arrangements based on rule
-func sweep(initial [][]byte, f rule, tolerance int) ([][]byte, bool) {
-	rows := len(initial)
-	cols := len(initial[0])
-
-	final := generateGrid(rows, cols)
-	duplicateGrid(final, initial)
-
-	for y := 0; y < rows; y++ {
-		for x := 0; x < cols; x++ {
-			// Check current seat using appropriate ruleset
-			countOccupied := f(initial, x, y, cols, rows)
-			if initial[y][x] == empty && countOccupied == 0 {
-				final[y][x] = occupied
-			} else if initial[y][x] == occupied && countOccupied >= tolerance {
-				final[y][x] = empty
-			}
-		}
-	}
-	// Convergence check - has grid changed?
-	status := checkGrid(initial, final)
-	return final, status
-}
-
-func show(initial [][]byte) {
-	for y := 0; y < len(initial); y++ {
-		for x := 0; x < len(initial[0]); x++ {
-			fmt.Printf("%c", initial[y][x])
-		}
-		fmt.Printf("\n")
-	}
-	fmt.Printf("\n")
-}
-
-func parseInputs(inputs []string) [][]byte {
+func parseInputs(inputs []string) *Seating {
 	var data [][]byte
 	for _, line := range inputs {
 		if len(line) > 0 {
 			data = append(data, []byte(line))
 		}
 	}
-	return data
+	seating := &Seating{}
+	seating.rows = len(data)
+	seating.cols = len(data[0])
+	seating.initial = data
+	seating.final = generateGrid(seating.rows, seating.cols)
+	duplicateGrid(seating.final, seating.initial)
+	return seating
 }
 
 func readInputs(filename string) []string {
